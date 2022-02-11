4b12ddddb10528b423b7ed088f154bb96fc7de24
rskhakha021 <rskhakha021@student.wethinkcode.co.za>
2022-02-11 12:17:44 +0200

    updated create_event.py

--- create_event.py
+++ create_event.py
@@ -1,20 +1,27 @@
 from datetime import datetime, timedelta
+from pydoc import describe
 from cal_setup import get_calendar_service
+import termtables as tt
 
 
 def main():
-   # creates one hour event tomorrow 10 AM IST
+   # creates one timed event for a given date
    service = get_calendar_service()
 
    d = datetime.now().date()
-   tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
-   start = tomorrow.isoformat()
-   end = (tomorrow + timedelta(hours=1)).isoformat()
+   summary = input("Enter event summary: ")
+   description = input("Enter event description: ")
+   hour  = int(input('At what hour is the event(enter 2 digits only)? '))
+   Days = int(input("Provide a day , enter 0 for today or 1 for tomorrow: "))
+   duration = float(input('How long is the event in hours? '))
+   event_details = datetime(d.year, d.month, d.day, hour)+timedelta(days=Days)
+   start = event_details.isoformat()
+   end = (event_details + timedelta(hours=duration)).isoformat()
 
    event_result = service.events().insert(calendarId='primary',
        body={
-           "summary": 'Another dummy test with python',
-           "description": 'This is a test event with google api for python',
+           "summary": summary,
+           "description": description,
            "start": {"dateTime": start, "timeZone": '+2'},
            "end": {"dateTime": end, "timeZone": '+2'},
        }
@@ -26,5 +33,15 @@ def main():
    print("starts at: ", event_result['start']['dateTime'])
    print("ends at: ", event_result['end']['dateTime'])
 
-if __name__ != '__main__':
-   main()
+
+
+
+####################################################################################################
+#delete event
+# def remove_event(event_id):
+    
+#     service= get_calendar_service()
+#     service.events().delete(calendarId='primary', eventId=event_id).execute()
+    
+# dd =  '0kd3hkpfu5a0s1cgsdvdkmsni4'
+# remove_event(dd) 

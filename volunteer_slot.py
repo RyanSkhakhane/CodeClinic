1ad7a63d3103cbb60b67f5c4fe1a1c88cd99b037
Ntethe Mokoena <ntmokoen021@student.wethinkcode.co.za>
2022-02-17 06:42:02 +0000

    Update volunteer_slot.py

--- volunteer_slot.py
+++ volunteer_slot.py
@@ -3,35 +3,56 @@ from pydoc import describe
 from cal_setup import get_calendar_service
 import termtables as tt
 from colorama import Fore, Back, Style
+import time
+import emoji
 
-def volunteer():
-   # creates one timed event for a given date
+
+
+def volunteer_slot():
+   """
+   function for handling volunteering of a slot.
+   """
+   
    service = get_calendar_service()
+   try:
 
-   d = datetime.now().date()
-   summary = input("Enter event summary: ")
-   description = input("Enter event description: ")
-   hour  = int(input('At what hour is the event(enter 2 digits only)? '))
-   Days = int(input("Provide a day , enter 0 for today or 1 for tomorrow: "))
-   duration = float(0.5)
-   event_details = datetime(d.year, d.month, d.day, hour)+timedelta(days=Days)
-   start = event_details.isoformat()
-   end = (event_details+timedelta(hours=duration)).isoformat()
+        d = datetime.now().date()
+        print()
+        print("To volunteer, you can create an event by following these steps below")
+        print()
+        
+        summary = input("Enter event summary: ")
+        
+        description = input("Enter event description: ")
+        
+        hour  = int(input('At what hour is the event(enter 2 digits only)? '))
+        
+        Days = int(input("Provide a day , enter 0 for today or 1 for tomorrow: "))
+        print()
+        duration = float(0.5)
+        event_details = datetime(d.year, d.month, d.day, hour)+timedelta(days=Days)
+        start = event_details.isoformat()
+        end = (event_details+timedelta(hours=duration)).isoformat()
 
-   event_result = service.events().insert(calendarId='primary' and "f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com",
-       body={
-           "summary": summary,
-           "description": description,
-           "start": {"dateTime": start, "timeZone": '+2'},
-           "end": {"dateTime": end, "timeZone": '+2'},
-       }
-   ).execute()
+        event_result = service.events().insert(calendarId='primary' and "f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com",
+            body={
+                "summary": summary,
+                "description": description,
+                "start": {"dateTime": start, "timeZone": '+2'},
+                "end": {"dateTime": end, "timeZone": '+2'},
+            }
+        ).execute()
 
-   print("summary: ", event_result['summary'])
-   print("starts at: ", event_result['start']['dateTime'])
-   print("ends at: ", event_result['end']['dateTime'])
-   print("id: ", event_result['id'])
-   print(Fore.LIGHTGREEN_EX+"Created event successfully")
+        print("Summary: ", event_result['summary'])
+        print("Event description: ", event_result['description'])
+        print("Starts at: ", event_result['start']['dateTime'])
+        print("Ends at: ", event_result['end']['dateTime'])
+      
+        print(Fore.LIGHTGREEN_EX+"Event created successfully",'\U0001f44d')
 
-if __name__ != "__main__":
-    volunteer()
+   except ValueError:
+      print("Please use valid hour inputs, e,g 9,12,13,14,23")
+      pass
+
+volunteer_slot()
+

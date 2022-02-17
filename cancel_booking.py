ee697e040d227ada00cee37debce2b577af8cb82
ntmokoen021 <ntmokoen021@student.wethinkcode.co.za>
2022-02-17 11:06:13 +0200

    booking

--- cancel_booking.py
+++ cancel_booking.py
@@ -11,25 +11,28 @@ def cancel_bookings():
    this function cancels the booked slot.
    
    """
-
-   service = get_calendar_service()
+   try:
+      service = get_calendar_service()
+      
+      d = datetime.now().date()
+      user_email = input("Please provide your email address:  ")
+      list_events.main(list_events.events_list, list_events.events_ID_list)
+      print()
+      
+      book = input('Enter event Id of the event you want to remove yourself from: ')
+      
    
-   d = datetime.now().date()
-   user_email = input("Please provide your email address:  ")
-   list_events.main(list_events.events_list, list_events.events_ID_list)
-   print()
-   
-   book = input('Enter event Id of the event you want to remove yourself from: ')
-   
-  
-   event = service.events().get(calendarId='f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com', eventId=book).execute()
+      event = service.events().get(calendarId='f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com', eventId=book).execute()
 
 
-   event['attendees'] = []  #here put the email of the person making the booking 
+      event['attendees'] = []  #here put the email of the person making the booking 
 
-   update_event = service.events().update(calendarId='f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com', eventId=book, body=event).execute()
+      update_event = service.events().update(calendarId='f2kecidvtbk56k8j6885l6pkso@group.calendar.google.com', eventId=book, body=event).execute()
 
-   print (update_event['updated'])
-   print("You have updated the event")
+      print (update_event['updated'])
+      print("You have updated the event")
+   except:
+      print("Please check your inputs again")
+      pass
 
 cancel_bookings()
